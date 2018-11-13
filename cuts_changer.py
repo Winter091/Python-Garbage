581b24e4461204dd3f0b08657e5737c7739018e9
Winter091 <winter091@yandex.ru>
2018-11-13 15:02:08 +0300

    Some small improvements

--- cuts_changer.py
+++ cuts_changer.py
@@ -3,11 +3,11 @@ import random
 
 parser = argparse.ArgumentParser(description='A script which replaces string cuts')
 
-parser.add_argument('--random', help='1 for random russian words, 0 for your choice', required=True)
-parser.add_argument('--input', default='input.txt', help='Input file name')
-parser.add_argument('--output', default='output.txt', help='Output file name')
-parser.add_argument('--old_cut', default=',', help='A string to be replaced')
-parser.add_argument('--new_cut', default=', блять,', help='A string which will replace the old')
+parser.add_argument('-r', '--random', help='Add random words instead of your choice', action='store_true')
+parser.add_argument('-i', '--input', default='input.txt', help='Input file name',)
+parser.add_argument('-o', '--output', default='output.txt', help='Output file name')
+parser.add_argument('-a', '--old_cut', default=',', help='A string to be replaced')
+parser.add_argument('-b', '--new_cut', default=', блять,', help='A string which will replace the old')
 
 args = vars(parser.parse_args())
 
@@ -18,17 +18,24 @@ words = (
 last_word = ''
 one_more_word = ''
 
+try:
+    with open(args['input'], 'r', encoding='UTF-8') as f:
+        pass
+except FileNotFoundError:
+    print('File', args['input'], 'is not found.')
+    exit(0)
+
 with open(args['input'], 'r', encoding='UTF-8') as f:
     string = f.read()
 
     if not int(args['random']):
         string = string.replace(args['old_cut'], args['new_cut'])
     else:
-        b = 0
+        offset = 0
 
         while True:
             try:
-                a = string.index(args['old_cut'], b)
+                a = string.index(args['old_cut'], offset)
             except ValueError:
                 break
 
@@ -48,7 +55,7 @@ with open(args['input'], 'r', encoding='UTF-8') as f:
             if word.endswith(('блять', 'бля', 'еблан', 'залупа', 'пизда', 'сука')):
                 word = word + ','
 
-            b = a + len(word) + 2
+            offset = a + len(word) + 2
 
             string = string[:a + 1] + ' ' + word + string[a + 1:]
             print('Adding word:', word)
