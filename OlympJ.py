0f27b35c75af0c3fdf7359abf1071b2bb47ddd48
Winter091 <33420667+Winter091@users.noreply.github.com>
2018-12-21 23:59:30 +0300

    Update stupid code so the code isn't so stupid now

--- OlympJ.py
+++ OlympJ.py
@@ -1,9 +1,7 @@
-a = input().split()
-if int(a[0]) * int(a[1]) < int(a[2]) * int(a[3]):
+a = [int(i) for i in input().split()]
+
+if a[0] * a[1] < a[2] * a[3]:
     print('P')
 else:
-    b = 'M' if int(a[0]) * int(a[1]) > int(a[2]) * int(a[3]) else 'E'
+    b = 'M' if a[0] * a[1] > a[2] * a[3] else 'E'
     print(b)
-
-for i in range(10,step=-1):
-    print(i)
